c60b3de6634157fc445a2dca85dfe839741e9ea3
monaemhossain <monaemhs@gmail.com>
2023-09-26 20:58:04 +0300

    corrected the 404 error massage

--- src/Pages/NotFoundPage/NotFound.jsx
+++ src/Pages/NotFoundPage/NotFound.jsx
@@ -4,7 +4,7 @@ const NotFound = () => {
     return (
         <div className="p-4 h-screen max-w-screen-xl mx-auto bg-white grid md:grid-cols-2 grid-cols-1 justify-center items-center">
             <div className="max-md:order-2 max-md:text-center">
-                <h2 className="text-5xl"><span className="text-7xl text-[#ffc727]">Sorry!!!</span><br />We Are Could n&apos;t Find Your Page.</h2>
+                <h2 className="text-5xl"><span className="text-7xl text-[#ffc727]">Sorry!!!</span><br />We Are Couldn&apos;t Find The Page.</h2>
                 <p>
                     <NavLink className='px-6 py-4 bg-[#ffc727] text-xl font-medium inline-block mt-6 hover:bg-white text-black hover:border-2 border-2 border-transparent hover:border-black transition-all' to='/'>Go To Home</NavLink>
                 </p>
